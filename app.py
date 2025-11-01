3677cb6b0d1851970e1618abf7c668adeefd7472
israelobi001 <israelobisesan44@gmail.com>
2025-11-01 16:02:54 +0000

    Ready for Render deploy

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import hashlib
 import qrcode
+from flask import Flask
 from flask import send_file, Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
